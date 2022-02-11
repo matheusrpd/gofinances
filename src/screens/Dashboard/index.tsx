c9a2bde1f9c6ac143524ccf5386ee2c81c451775
matheusrpd <matheusrpd@gmail.com>
2022-02-11 14:35:51 -0300

    Finalizado cards de destaques e criado lista de transações.

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,10 @@
 import { HighlightCard } from '../../components/HighlightCard';
 import {
+	TransactionCard,
+	TransactionCardData,
+} from '../../components/TransactionCard';
+
+import {
 	Container,
 	Header,
 	UserWrapper,
@@ -10,9 +15,43 @@ import {
 	UserName,
 	Icon,
 	HighlightCards,
+	Transactions,
+	Title,
+	TransactionsList,
 } from './styles';
 
+export interface CardListProps extends TransactionCardData {
+	id: string;
+}
+
 export function Dashboard() {
+	const data: CardListProps[] = [
+		{
+			id: '1',
+			type: 'positive',
+			title: 'Desenvolvimento de site',
+			amount: 'R$ 12.000,00',
+			date: '10/02/2022',
+			category: { name: 'Vendas', icon: 'dollar-sign' },
+		},
+		{
+			id: '2',
+			type: 'negative',
+			title: 'Hambugueria Pizzy',
+			amount: 'R$ 59,00',
+			date: '07/02/2022',
+			category: { name: 'Alimentação', icon: 'coffee' },
+		},
+		{
+			id: '3',
+			type: 'negative',
+			title: 'Aluguel do apartamento',
+			amount: 'R$ 1.200,00',
+			date: '05/02/2022',
+			category: { name: 'Casa', icon: 'shopping-bag' },
+		},
+	];
+
 	return (
 		<Container>
 			<Header>
@@ -30,10 +69,35 @@ export function Dashboard() {
 			</Header>
 
 			<HighlightCards>
-				<HighlightCard />
-				<HighlightCard />
-				<HighlightCard />
+				<HighlightCard
+					type="up"
+					title="Entradas"
+					amount="R$ 17.400,00"
+					lastTransaction="Última entrada dia 10 de fevereiro"
+				/>
+				<HighlightCard
+					type="down"
+					title="Saídas"
+					amount="R$ 17.400,00"
+					lastTransaction="Última entrada dia 10 de fevereiro"
+				/>
+				<HighlightCard
+					type="total"
+					title="Total"
+					amount="R$ 17.400,00"
+					lastTransaction="Última entrada dia 10 de fevereiro"
+				/>
 			</HighlightCards>
+
+			<Transactions>
+				<Title>Listagem</Title>
+
+				<TransactionsList
+					data={data}
+					keyExtractor={(item) => item.id}
+					renderItem={({ item }) => <TransactionCard data={item} />}
+				/>
+			</Transactions>
 		</Container>
 	);
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components/native';
+import { FlatList, FlatListProps } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { CardListProps } from './index';
 
 export const Container = styled.View`
   flex: 1;
@@ -9,14 +13,17 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   width: 100%;
-  justify-content: center;
+  justify-content: flex-start;
   height: ${RFPercentage(42)}px;
   background-color: ${({ theme }) => theme.colors.primary};
 `;
 
 export const UserWrapper = styled.View`
   width: 100%;
+
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -25,7 +32,6 @@ export const UserWrapper = styled.View`
 export const UserInfo = styled.View`
   flex-direction: row;
   align-items: center;
-
 `;
 
 export const Photo = styled.Image`
@@ -59,4 +65,29 @@ export const HighlightCards = styled.ScrollView.attrs({
   horizontal: true,
   showsHorizontalScrollIndicator: false,
   contentContainerStyle: { paddingLeft: 24, paddingRight: 8 } // 8 na direita pois já tem 16 do card
-})``; 
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px; 
+`; 
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 ${RFValue(24)}px;
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  margin-bottom: 16px;
+`;  
+
+export const TransactionsList = styled(
+  FlatList as new (props: FlatListProps<CardListProps>) => FlatList<CardListProps>)
+  .attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: {
+      paddingBottom: getBottomSpace(),
+    }
+})``;
